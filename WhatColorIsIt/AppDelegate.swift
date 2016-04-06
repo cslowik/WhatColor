9e69ff2b16ee2cc45eea8decb06f8f71fadb04ee
Chris Slowik <chris.slowik@gmail.com>
2016-04-06 00:19:14 -0700

    added light mode style

--- WhatColorIsIt/AppDelegate.swift
+++ WhatColorIsIt/AppDelegate.swift
@@ -11,27 +11,35 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    let uDefaults = NSUserDefaults.standardUserDefaults()
+    
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
     var timer: NSTimer!
+    var appearance: String!
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         statusItem.menu = statusMenu
-        updateTime()
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        appearance = uDefaults.stringForKey("AppleInterfaceStyle") ?? "Light"
+        //uDefaults.addObserver(self, forKeyPath: "AppleInterfaceStyle", options: NSKeyValueObservingOptions.New, context: nil)
+        
+        
+        updateTime()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        timer.invalidate()
+        //uDefaults.removeObserver(self, forKeyPath: "AppleInterfaceStyle")
     }
 
     @IBAction func quitClicked(sender: NSMenuItem) {
-        timer.invalidate()
         NSApplication.sharedApplication().terminate(self)
     }
     
     func updateTime() {
-        statusItem.image = NSImage.whatColor()
+        statusItem.image = NSImage.whatColorIcon(appearance)
     }
 
 }
@@ -49,12 +57,16 @@ extension NSColor {
 }
 
 extension NSImage {
-    class func whatColor() -> NSImage {
+    class func whatColorIcon(appearance: String) -> NSImage {
         let theIcon = NSImage(size: NSSize(width: 16, height: 16))
         theIcon.lockFocus()
         let iconPath = NSBezierPath(roundedRect: NSMakeRect(0, 0, 14, 14), xRadius: 3, yRadius: 3)
+        if appearance == "Dark" {
+            NSColor(red:0.133, green:0.152, blue:0.182, alpha:0.5).setStroke()
+        } else {
+            NSColor(red:0.941, green:0.941, blue:0.941, alpha:0.5).setStroke()
+        }
         NSColor.fromTime().setFill()
-        NSColor(red:0.133, green:0.152, blue:0.182, alpha:0.5).setStroke()
         iconPath.fill()
         iconPath.stroke()
         theIcon.unlockFocus()
